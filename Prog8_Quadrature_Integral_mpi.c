303b22b0475262d318a0087e1f3cc37b01f81cc6
Faizan-Mushtaq <faizan.wani234@gmail.com>
2021-02-10 00:57:30 +0530

    Correct Program8 for all values of a nad b

--- Prog8_Quadrature_Integral_mpi.c
+++ Prog8_Quadrature_Integral_mpi.c
@@ -1,60 +1,65 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<mpi.h>
+#include<math.h>
 
 double f(double x){
-    double pi = 3.14;
+    double pi = 3.141592653589793;
     return (double)(50/(pi*(2500*x*x + 1)));
 }
-
-int main(int argc, char **argv){
-    double t;
-    int p, id;
-    int n = 100000;
-    int my_n;
-    double exact = 0.49963;
-    double error;
-    double my_a, my_b, my_total = 0.0, total = 0.0, a = 0.0, b = 10.0;
-
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &id);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-    double intvl = b/(double)(p-1);
-    if(id == 0){
-        printf("Computing the value of integral f(x) = 50/(pi*(2500*x*x + 1))");
-        t = MPI_Wtime();
-        my_n = n/(p-1);
+int main(int argc,char **argv){
+  double a=0.0,b=10.0;
+  int n=10000000;
+  double exact=0.499363381076456;
+  MPI_Status status;
+  int rank,p,i;
+  int target,tag;
+  double time,error;
+  double my_a,my_b,my_n,x;
+  double total=0.0,my_total=0.0;
+  int src;
+  MPI_Init(&argc,&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Comm_size(MPI_COMM_WORLD,&p);
+  if(rank==0)
+  {
+    time=MPI_Wtime();
+    my_n=n/(p-1);
+    n=(p-1)*my_n;
+  }
+  MPI_Bcast(&my_n,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
+  if(rank==0){
+    for(int i=1;i<p;i++){
+      my_a=((p-i)*a +(i-1)*b)/(double)(p-1);
+      my_b=((p-i-1)*a +(i*b))/(double)(p-1);
+      target=i;//process to which we send a and b
+      tag=1;
+      MPI_Send(&my_a,1,MPI_DOUBLE,target,tag,MPI_COMM_WORLD);
+      target=i;tag=2;
+      MPI_Send(&my_b,1,MPI_DOUBLE,target,tag,MPI_COMM_WORLD);
     }
-    MPI_Bcast(&my_n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    if(id == 0){
-        int i;
-        for(i=1;i<=p-1;i++){
-            my_a = i*intvl - intvl;
-            my_b = i*intvl;
-            printf("%d %f %f\n",i, my_a,my_b);
-            MPI_Send(&my_a, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&my_b, 1, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
-        }
+    total=0.0;
+    my_total=0.0;
+  }
+  else{//workers
+    tag=1;src=0;
+    MPI_Recv(&my_a,1,MPI_DOUBLE,src,tag,MPI_COMM_WORLD,&status);
+    tag=2;src=0;
+    MPI_Recv(&my_b,1,MPI_DOUBLE,src,tag,MPI_COMM_WORLD,&status);
+    my_total=0.0;
+    for(int i=1;i<my_n;i++){
+      x=((my_n-i)*my_a + (i-1)*my_b)/(double)(my_n-1);
+      my_total+=f(x);
     }
-    if(id>0){
-        MPI_Status status;
-        int i;
-        double x;
-        printf("%d %d\n",id,my_n);
-        MPI_Recv(&my_a, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
-        MPI_Recv(&my_b, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
-        for(i=0;i<my_n;i++){
-            x = ((my_n - i)*my_a + (i-1)*my_b)/(double)(my_n-1);
-            my_total+=f(x);
-        }
-        my_total = (my_b-my_a)*my_total/(double)(my_n);
-    }
-    MPI_Reduce(&my_total, &total, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    if(id == 0){
-        error = exact - total;
-        printf("Integral computed is %f with error %f in time %f",total,error,MPI_Wtime()-t);
-    }
-    MPI_Finalize();
+    my_total=(my_b-my_a)*my_total/(double)(my_n);
+    printf("Process %d contribution= %f\n",rank,my_total);
+  }
+  MPI_Reduce(&my_total,&total,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+  if(rank==0){
+    error=fabs(exact-total);
+    time=MPI_Wtime()-time;
+    printf("Error = %f\nTime Taken= %f\n",error,time);
+  }
+  MPI_Finalize();
+  return 0;
 }
